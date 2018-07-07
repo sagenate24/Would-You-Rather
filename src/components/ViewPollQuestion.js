cb860cbf7a00bf9e5dfbbab85c721c7838b49078
Nate Sage <davidsage@Davids-MacBook-Pro.local>
2018-07-06 18:50:07 -0700

    feat: added basic ui to the and/or question part of the game

--- src/components/ViewPollQuestion.js
+++ src/components/ViewPollQuestion.js
@@ -3,34 +3,54 @@ import { connect } from 'react-redux';
 import { formatPollQuestion } from '../utils/helpers';
 
 class PollQuestion extends Component {
+  state = {
+    value: '',
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    alert(this.state.value);
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      value: e.target.value
+    })
+  }
+
   render() {
     const { author, question, id } = this.props;
-    console.log(this.props)
 
     return (
       <div>
-        <img src={author.avatarURL} alt={'avatar'} height={'100px'}/>
+        <img src={author.avatarURL} alt={'avatar'} height={'100px'} />
         <div>
           <p>{author.name} asks:</p>
           <h3>Would You Rather?</h3>
         </div>
         <div>
-          <select>
-            <option></option>
-            <option></option>
-          </select>
+          <form onSubmit={this.handleSubmit}>
+            <select defaultValue='none' onChange={this.handleChange}>
+              <option disabled value='none'>?</option>
+              <option value='optionOne'>{question.optionOne.text}</option>
+              <option disabled>OR</option>
+              <option value='optionTwo'>{question.optionTwo.text}</option>
+            </select>
+            <button type='submit'>Submit</button>
+          </form>
         </div>
       </div>
     )
   }
 }
 
-function mapStateToProps ({ authedUser, questions, users }, props) {
+function mapStateToProps({ authedUser, questions, users }, props) {
   const { id } = props.match.params
 
   return {
     id,
-    question : questions[id],
+    question: questions[id],
     author: users[questions[id].author]
   }
 }
